e4092c792fe919533b1147920ffd975552f22e33
Victor Navarro <victor.matias.navarro@gmail.com>
2018-01-29 17:46:43 -0200

    Making PreProjeto generate CSV
    Signed-off-by: Marcos Ronaldo <marcos.rpj2@gmail.com>
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>

--- salic_api/resources/preprojeto/pre_projeto_detail.py
+++ salic_api/resources/preprojeto/pre_projeto_detail.py
@@ -7,6 +7,12 @@ from ..serialization import listify_queryset
 class PreProjetoDetail(DetailResource):
     query_class = PreProjetoQuery
     resource_path = 'propostas'
+    csv_columns = ['impacto_ambiental', 'ficha_tecnica', 'data_termino', 'id',
+                    'mecanismo', 'data_arquivamento', 'data_inicio',
+                    'democratizacao', 'data_aceite', 'sinopse',
+                    'nome', 'estrategia_execucao', 'especificacao_tecnica',
+                    'acessibilidade', 'objetivos', 'etapa', 'resumo',
+                    'justificativa']
 
     def hal_links(self, result):
         return {
--- tests/test_csv.py
+++ tests/test_csv.py
@@ -4,11 +4,15 @@ class TestCsv:
         expected = INCENTIVADOR_CSV
         check_csv(client, url, expected)
 
-
     def test_proponente_csv(self, client):
     	url = '/v1/proponentes/30313233343536373839616263646566e0797636'
     	expected = PROPONENTE_CSV
     	check_csv(client, url, expected)
+
+    def test_preprojeto_csv(self, client):
+        url = '/v1/propostas/1'
+        expected = PROPOSTA_CSV
+        check_csv(client, url, expected)
 
 
 def check_csv(client, url, expected):
@@ -28,3 +32,14 @@ INCENTIVADOR_CSV = """cgccpf,nome,responsavel,tipo_pessoa,UF,municipio,total_doa
 PROPONENTE_CSV = """cgccpf,nome,responsavel,tipo_pessoa,UF,total_captado,municipio
 1234,Nome,Responsavel,juridica,Uf,1000,Cidade
 """.replace('\n', '\r\n')
+
+PROPOSTA_CSV = (
+    "impacto_ambiental,ficha_tecnica,data_termino,id,mecanismo,"
+    "data_arquivamento,data_inicio,democratizacao,data_aceite,sinopse,nome,"
+    "estrategia_execucao,especificacao_tecnica,acessibilidade,objetivos,etapa,"
+    "resumo,justificativa\r\n"
+
+    "ImpactoAmbiental,FichaTecnica,2000-02-01,1,Descricao,2000-03-01,2000-01-01,"
+    "DemocratizacaoDeAcesso,2000-01-01,Sinopse,Test,EstrategiadeExecucao,"
+    "EspecificacaoTecnica,Acessibilidade,cultural,"
+    "EtapaDeTrabalho,ResumoDoProjeto,Justificativa\r\n")
