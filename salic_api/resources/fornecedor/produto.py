ec90bbb460fbe7f467158624ac2f1d0b71717710
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2018-02-20 18:04:43 +0000

    Add links on produto test

--- salic_api/resources/fornecedor/produto.py
+++ salic_api/resources/fornecedor/produto.py
@@ -1,12 +1,22 @@
 from flask import current_app
 from .query import ProductQuery
-from ..resource import ListResource
+from ..resource import DetailResource, ListResource
 from ...utils import encrypt, decrypt
 
+
+class ProdutoDetail(DetailResource):
+
+    def hal_links(self, result):
+        fornecedor_id = self.args['fornecedor_id']
+        return {
+            'fornecedor': self.url('/fornecedores/%s' % fornecedor_id),
+        }
 
 class Produto(ListResource):
     query_class = ProductQuery
     embedding_field = 'produtos'
+    detail_resource_class = ProdutoDetail
+    detail_pk = 'cgccpf'
 
     def build_query_args(self):
         args = super().build_query_args()
@@ -17,38 +27,3 @@ class Produto(ListResource):
     @property
     def resource_path(self):
         return "%s/%s/%s" % ("fornecedores", self.args['fornecedor_id'], 'produtos')
-
-    # def build_links(self, args={}):
-
-    #     self.projetos_links = []
-
-    #     for PRONAC in args['projetos_PRONAC']:
-    #         link = current_app.config['API_ROOT_URL'] + 'projetos/%s/' % PRONAC
-    #         self.projetos_links.append(link)
-
-    #     self.fornecedor_links = []
-
-    #     for fornecedor_id in args['fornecedor_id']:
-    #         url_id = encrypt(fornecedor_id)
-    #         link = current_app.config['API_ROOT_URL'] + \
-    #             'fornecedores/?url_id=%s' % url_id
-    #         self.projetos_links.append(link)
-
-    # def hal_builder(self, data, args=None):
-    #     produtos = []
-
-    #     for index in range(len(data['produtos'])):
-
-    #         produtos = data['produtos'][index]
-
-    #         projeto_link = self.projetos_links[index]
-    #         fornecedor_link = self.fornecedor_links[index]
-
-    #         produtos['_links'] = {}
-    #         produtos['_links']['projeto'] = projeto_link
-    #         produtos['_links']['fornecedor'] = fornecedor_link
-
-    #     data['_embedded'] = {'produtos': produtos}
-    #     del data['produtos']
-
-    #     return data
--- tests/examples.py
+++ tests/examples.py
@@ -233,6 +233,10 @@ PRODUTOS_RESPONSE = {
                 "tipo_forma_pagamento": 'Dinheiro',
                 "data_aprovacao": '2000-02-02',
                 "valor_pagamento": 2000.0,
+                "_links": {
+                #     "projeto": "http://api.salic.cultura.gov.br/v1/projetos/148794",
+                    "fornecedor": "v1/fornecedores/30313233343536373839616263646566e0797636"
+                },
                 "id_arquivo": 1,
                 "nr_comprovante": '1',
                 "nome_fornecedor": "Name",
