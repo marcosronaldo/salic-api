4e40ee85672198fd762ad4597991804b33285be3
Bruna Pinos <bruna.pinos@gmail.com>
2018-02-21 09:38:26 -0300

    Adds more Base fixtures

--- salic_api/fixtures/base.py
+++ salic_api/fixtures/base.py
@@ -4,15 +4,17 @@ from datetime import datetime
 
 from salic_api.models import Prorrogacao
 
-
-def prorrogacao_example():
+def prorrogacao_example(size=1):
     return [Prorrogacao(
-        idProrrogacao=1,
-        Logon=1,
+        idProrrogacao=i,
+        Logon=i,
         DtPedido=datetime(2000, 1, 1),
         DtInicio=datetime(2000, 1, 1),
         DtFinal=datetime(2000, 3, 1),
         Observacao='Observacao',
         Atendimento='A',
-        idPronac=20001234,
-    )]
+        idPronac=20001234)
+        for i in range(1,size+1)
+    ]
+
+
